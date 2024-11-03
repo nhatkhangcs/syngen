400b5206734ba6c0d82225c8b6482f72cadd480d
Tran Quoc Hieu <ngoquochieu1211@gmail.com>
2024-11-03 10:22:13 +0700

    feat: build class condition

--- src/syngen/condition/__init__.py
+++ src/syngen/condition/__init__.py
@@ -1,6 +1,21 @@
 from .base import *
+from .distribution import (
+    UniformDistribution,
+    NormalDistribution,
+    BinomialDistribution,
+    PoissonDistribution,
+    ExponentialDistribution,
+    CustomDistribution,
+)
 
 __all__ = [
     'BaseCondition',
     'Condition',
+    'UniformDistribution',
+    'NormalDistribution',
+    'BinomialDistribution',
+    'PoissonDistribution',
+    'ExponentialDistribution',
+    'CustomDistribution',
 ]
+
--- src/syngen/condition/base.py
+++ src/syngen/condition/base.py
@@ -1,12 +1,19 @@
-class BaseCondition:
+import numpy as np
+from abc import ABC, abstractmethod
+
+
+class BaseCondition(ABC):
+
     def __init__(self):
         pass
 
 
-class Condition(BaseCondition):
+class Condition(BaseCondition, ABC):
+
     def __init__(self):
         super().__init__()
         pass
 
+    @abstractmethod
     def __getitem__(self, item):
         pass
